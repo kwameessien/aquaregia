838bd9494f4c4ab6abdf7f6d41e7c1df9a5e6805
kwameessien <180320488+kwameessien@users.noreply.github.com>
2026-05-03 00:34:53 -0400

    chore: remove stray page 2 starter; enlarge and lower rituals preorder CTA

--- src/components/home/RitualsPreorderCta.tsx
+++ src/components/home/RitualsPreorderCta.tsx
@@ -1,11 +1,11 @@
 import Link from "next/link";
 
 const linkClassName =
-  "font-site-nav text-[11px] font-normal uppercase tracking-[0.14em] text-black no-underline transition-colors hover:underline hover:decoration-black hover:decoration-1 hover:underline-offset-[3px]";
+  "font-site-nav text-[13px] font-normal uppercase tracking-[0.12em] text-black no-underline transition-colors hover:underline hover:decoration-black hover:decoration-1 hover:underline-offset-[3px] sm:text-[14px]";
 
 export default function RitualsPreorderCta() {
   return (
-    <p className="relative z-10 mt-[28px] w-full text-center sm:mt-[36px]">
+    <p className="relative z-10 mt-[48px] w-full text-center sm:mt-[60px]">
       <Link href="/rituals-book" className={linkClassName}>
         Pre-order here
       </Link>
